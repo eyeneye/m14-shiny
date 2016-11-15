2c55d2cf8a768a7a7773a667866a68fa0b01fb61
eyeneye <nathanbombardier@d-173-250-241-77.dhcp4.washington.edu>
2016-11-15 15:12:39 -0800

    did exercises

--- exercise-3/server.R
+++ exercise-3/server.R
@@ -1,8 +1,9 @@
 # server.R
 library(dplyr)
-
+library(plotly)
+library(shiny)
 # Read in data
-setwd('~/Documents/info-201/m14-shiny/exercise-3/')
+setwd('/Users/nathanbombardier/Documents/School/Fall2016/INFO201/m14-shiny/exercise-3')
 source('./scripts/buildMap.R')
 df <- read.csv('./data/electoral_college.csv', stringsAsFactors = FALSE)
 state.codes <- read.csv('./data/state_codes.csv', stringsAsFactors = FALSE)
@@ -12,12 +13,12 @@ joined.data <- left_join(df, state.codes, by="state")
 
 # Compute the electoral votes per 100K people in each state
 joined.data <- joined.data %>% mutate(ratio = votes/population * 100000)
-
 # Start shinyServer
+View(joined.data)
 shinyServer(function(input, output) { 
   
   # Render a plotly object that returns your map
   output$map <- renderPlotly({ 
-      return(BuildMap(joined.data, input$mapvar))
+    return(BuildMap(joined.data, input$mapvar))
   })
 })
